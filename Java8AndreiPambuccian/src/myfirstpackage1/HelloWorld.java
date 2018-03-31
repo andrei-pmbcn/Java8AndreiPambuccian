8713cf15fab98c5aff7314c3083f682abd90a43c
andrei-pmbcn <andreipmbcn@yahoo.com>
2018-03-31 18:44:04 +0300

    First assignment

--- Java8AndreiPambuccian/src/myfirstpackage1/HelloWorld.java
+++ Java8AndreiPambuccian/src/myfirstpackage1/HelloWorld.java
@@ -15,8 +15,66 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Salut lume!");
-
+        calc();
     }
     
+    static void calc() {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        
+        System.out.println("Please introduce your first number:");        
+        int x;
+        try {
+            x = sc.nextInt();
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("Invalid number.");
+            return;
+        }
+
+        System.out.println("Please introduce operator (+ - / *):");
+        String sop = sc.next();
+        String[] operators = {"+", "-", "/", "*"};
+        if (!java.util.Arrays.asList(operators).contains(sop)) {
+            System.out.println("Invalid operator.");
+            return;
+        }
+        
+        int y;
+        System.out.println("Please introduce your second number:");        
+        try {
+            y = sc.nextInt();
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("Invalid number.");
+            return;
+        }
+        
+        System.out.println("Please introduce the = sign:");
+        String seq = sc.next();
+        if (!"=".equals(seq)) {
+            System.out.println("Invalid input.");
+            return;
+        }
+        
+        int res;
+        
+        switch (sop) {
+            case "+":
+                res = x + y;
+                break;
+            case "-":
+                res = x - y;
+                break;
+            case "*":
+                res = x * y;
+                break;
+            case "/":
+                res = x / y;
+                break;
+            default:
+                res = 0;
+                break;
+        }
+        
+        System.out.print("Result: ");
+        System.out.print(res);   
+    }
 }
