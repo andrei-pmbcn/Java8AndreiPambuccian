fccc8447c5db2b52dc07416c362b70ebdbca40c2
andrei-pmbcn <andreipmbcn@yahoo.com>
2018-09-01 19:50:08 +0300

    First assignment

---
+++ Java9AndreiPambuccian/src/java9andreipambuccian/Java9AndreiPambuccian.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java9andreipambuccian;
+
+/**
+ *
+ * @author andrei
+ */
+public class Java9AndreiPambuccian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world!");
+    }
+    
+}
